d0b2a79f363e2039800930c4460f09c8493018b6
trilinhle1111 <letrilinh2103k4@gmail.com>
2023-02-20 09:57:22 +0700

    bai tap buoi 2

---
+++ vonglap/bai5.java
@@ -0,0 +1,27 @@
+package vonglap;
+import java.util.Scanner;
+public class bai5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhập 1 số bất kỳ:");
+        int number = sc.nextInt();
+        if (number < 2) {
+            System.out.println(number + " is not a prime");
+        } else {
+            int i = 2;
+            boolean a = true;
+            while (i < number) {
+                if (number % i == 0) {
+                    a = false;
+                    break;
+                }
+                i++;
+            }
+            if (a)
+                System.out.println(number + " is a prime");
+            else
+                System.out.println(number + " is not a prime");
+        }
+    }
+}
+
